804527ce8446a064c2710b614773c75045624f72
hollias <clous.namsoo@gmail.com>
2019-01-14 13:44:28 +0900

    first commit

---
+++ src/main/java/com/hollias/inflearn/intelliJ/view/ViewDoc.java
@@ -0,0 +1,9 @@
+package com.hollias.inflearn.intelliJ.view;
+
+public class ViewDoc {
+    public void viewDoc(){
+        double random = Math.random();
+        System.out.println("aaa");
+        System.out.println("aaa");
+    }
+}
