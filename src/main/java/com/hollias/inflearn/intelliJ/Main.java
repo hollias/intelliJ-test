428a7fcbb569578a8c3e93060d4987945478b90b
hollias <clous.namsoo@gmail.com>
2019-01-14 13:48:29 +0900

    second commit

---
+++ src/main/java/com/hollias/inflearn/intelliJ/Main.java
@@ -0,0 +1,10 @@
+package com.hollias.inflearn.intelliJ;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.print("aaaa");
+        System.out.println("aaaabb");
+        System.out.println("addsout");
+
+    }
+}
